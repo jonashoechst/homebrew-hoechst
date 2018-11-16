5b55468e3eefd740b582e32ddde1961007ad4154
Jonas Höchst <git@jonashoechst.de>
2018-11-16 17:46:07 +0100

    added EasyABC

---
+++ Casks/easyabc.rb
@@ -0,0 +1,10 @@
+cask 'easyabc' do
+  version '1.3.5'
+  sha256 '6169a2fa0adaa442b2f27799ae4bf632782c7f78e7255789324e6932deb8231c'
+
+  url 'https://www.nilsliberg.se/ksp/easyabc/EasyABC_1.3.5.dmg'
+  name 'EasyABC'
+  homepage 'https://www.nilsliberg.se/ksp/easyabc/'
+
+  app 'EasyABC.app'
+end
