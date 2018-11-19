dd1faced505b5512dc53889932c6f12e85fa88ee
Jonas Höchst <git@jonashoechst.de>
2018-11-19 15:24:20 +0100

    fixed version number

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,5 +1,5 @@
 cask 'audacity' do
-  version '2.1.1'
+  version '2.2.1'
   sha256 'a2a850ca956ad8ddf369e5bf251a739e966ebf1d57b19d88bd9a3da9121446fa'
 
   url 'https://www.audacity.de/download/audacity_mac.dmg'
