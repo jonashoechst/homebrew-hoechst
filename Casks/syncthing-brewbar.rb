1fa40bd3d129eb38fdca2987c4d0c2e677c0f3f4
Jonas Höchst <git@jonashoechst.de>
2020-01-22 11:21:19 +0100

    Syncthing BrewBar -> beta6

--- Casks/syncthing-brewbar.rb
+++ Casks/syncthing-brewbar.rb
@@ -1,6 +1,6 @@
 cask 'syncthing-brewbar' do
-  version 'beta5'
-  sha256 'f0d43249274b811c637cb401a689cef0f735550bf9c53004ddd03dd15079de0f'
+  version 'beta6'
+  sha256 '317da70220c56c7f8fa4cf1f3e23e9563f339c098aefecc972b9faef2e3c8cb6'
 
   url "https://github.com/jonashoechst/syncthing-brew-gui/releases/download/#{version}/Syncthing-BrewBar.zip"
   name 'Syncthing BrewBar'
