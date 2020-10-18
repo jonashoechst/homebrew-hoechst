43f84f352882a8e7ac2171db8de89658cffadffa
Jonas Höchst <git@jonashoechst.de>
2020-10-18 10:57:26 +0200

    version bump; extend config example

--- dtn7.rb
+++ dtn7.rb
@@ -2,8 +2,8 @@ class Dtn7 < Formula
   desc "Delay-tolerant networking software suite, Bundle Protocol Version 7"
   homepage "https://dtn7.github.io"
   url "https://github.com/dtn7/dtn7-go.git",
-      :tag      => "v0.5.4",
-      :revision => "69294c6eb269de128095099d74e34c3c8e6102c9"
+      :tag      => "v0.9.0",
+      :revision => "08ab34ba60d129bb0c10f69f6f13493bcead6d4e"
   head "https://github.com/dtn7/dtn7-go.git"
 
   depends_on "go" => :build
@@ -15,39 +15,167 @@ class Dtn7 < Formula
     src.install buildpath.children
     src.cd do
       system "go", "build", "-o", "#{name}d", "./cmd/dtnd/"
-      system "go", "build", "-o", "#{name}cat", "./cmd/dtncat/"
+      system "go", "build", "-o", "#{name}-tool", "./cmd/dtn-tool/"
       bin.install "#{name}d"
-      bin.install "#{name}cat"
+      bin.install "#{name}-tool"
 
       (src/"#{name}d.toml").write <<~EOS
-        [core]
-        store = "#{etc}/#{name}/store"
-        inspect-all-bundles = true
-        node-id = "dtn://#{Socket.gethostname}/"
+# SPDX-FileCopyrightText: 2019 Markus Sommer
+# SPDX-FileCopyrightText: 2019, 2020 Alvar Penning
+# SPDX-FileCopyrightText: 2020 Jonas Höchst
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-        [logging]
-        level = "info"
-        report-caller = false
-        format = "text"
+# The core is the main module of the delay-tolerant networking daemon.
+[core]
+# Path to the bundle storage. Bundles will be saved in this directory to be
+# present after restarting dtnd.
+store = "#{etc}/#{name}/store"
 
-        [discovery]
-        ipv4 = true
-        ipv6 = true
-        interval = 30
+# Allow inspection of forwarding bundles, containing an administrative record.
+# This allows deletion of stored bundles after being received.
+inspect-all-bundles = true
 
-        [simple-rest]
-        node = "dtn://#{Socket.gethostname}/"
-        listen = "127.0.0.1:8080"
+# The node's ID, which should be a dtn-URI. Each node's endpoint ID should be
+# an URI based on the given node-id.
+node-id = "dtn://#{Socket.gethostname}/"
 
-        [[listen]]
-        protocol = "tcpcl"
-        endpoint = ":4556"
+# If a signature-private entry exists, all outgoing bundles created at this
+# node will be signed with the following key. Such a key can be created by:
+#   $ xxd -l 64 -p -c 64 /dev/urandom
+# Please DO NOT use the following key or a variation of it. I am serious.
+# signature-private = "2d5b59df9e860636ee392fc7833d957543cd7e47e95b8a2800224408840242a8edff1aafc10af23ae32a6868e2c31cbbcf3157a706accae2eb7faa7a1d7ee84e"
 
-        [routing]
-        algorithm = "epidemic"
+
+# Configure the format and verbosity of dtnd's logging.
+[logging]
+# Should be one of, sorted from silence to verbose:
+# panic,fatal,error,warn,info,debug,trace
+level = "info"
+
+# Show the calling method and its file in the logs
+# report-caller = true
+
+# Could be "text" for human readable output or "json".
+# format = "json"
+
+
+# The peer/neighbor discovery searches the (local) network for other dtnd nodes
+# and tries to establish a connection to the promoted CLAs.
+[discovery]
+ipv4 = true
+ipv6 = true
+
+# Interval between two messages in seconds, defaults to 10.
+interval = 30
+
+
+# Agents are applications or interfaces for sending or receiving bundles.
+[agents]
+# Web server based agent with an own HTTP server for third party tools.
+[agents.webserver]
+# Address to bind the server to.
+address = "localhost:8080"
+
+# Create a WebSocket endpoint at "ws://localhost:8080/ws"
+websocket = true
+
+# Create a RESTful endpoints at "http://localhost:8080/rest/"
+rest = true
+
+
+# Each listen is another convergence layer adapter (CLA). Multiple [[listen]]
+# blocks are usable.
+[[listen]]
+# Protocol to use, one of tcpclv4, tcpclv4-ws, mtcp, bbc.
+protocol = "tcpclv4"
+
+# Address to bind this CLA to.
+endpoint = ":4556"
+
+
+# Another example based on the WebSocket variant of the TCPCLv4.
+# [[listen]]
+# protocol = "tcpclv4-ws"
+# # Webserver on port 8081 with a WebSocket endpoint at "ws://HOST:8081/tcpclv4".
+# endpoint = ":8081"
+
+
+# Another example for a Bundle Broadcasting Connector with a rf95modem.
+# [[listen]]
+# protocol = "bbc"
+# endpoint = "bbc://rf95modem/dev/ttyUSB0"
+
+
+# Multiple [[peers]] might be configured.
+# [[peer]]
+# # Protocol to use, one of tcpclv4, tcpclv4-ws, mtcp.
+# protocol = "tcpclv4"
+# # Address to connect to this CLA.
+# endpoint = "10.0.0.2:4556"
+
+
+# [[peer]]
+# protocol = "tcpclv4-ws"
+# endpoint = "ws://HOST:PORT/tcpclv4"
+
+
+# Another peer example..
+# [[peer]]
+# # The name/endpoint ID of this peer, as MTCP does not support any introduction.
+# node = "dtn://gamma/"
+# protocol = "mtcp"
+# endpoint = "[fc23::2]:35037"
+
+
+# Specify routing algorithm
+[routing]
+# One of  "epidemic", "spray", "binary_sparay", "dtlsr", "prophet", "sensor-mule"
+algorithm = "epidemic"
+
+
+# Config for spray routing
+# [routing.sprayconf]
+# multiplicity = 10
+
+
+# Config for dtlsr
+# [routing.dtlsrconf]
+# recomputetime = "30s"
+# broadcasttime = "30s"
+# purgetime = "10m"
+
+
+# Config for prophet
+# [routing.prophetconf]
+# # pinit ist the prophet initialisation constant (default value provided by the PROPHET-paper)
+# pinit = 0.75
+#
+# # beta is the prophet scaling factor for transitive predictability (default value provided by the PROPHET-paper)
+# beta = 0.25
+#
+# # gamma is the prophet ageing factor (default value provided by the PROPHET-paper)
+# gamma = 0.98
+#
+# ageinterval = "1m"
+
+
+# Config for sensor-mule
+# [routing.sensor-mule-conf]
+# # sensor-node-regex is a regular expression matching sensor node's node IDs.
+# sensor-node-regex = "^dtn://[^/]+\\.sensor/.*$"
+#
+# # This routing structure defines the underlying routing algorithm;
+# # it's identical to the parent routing section.
+# # In this example, the underlying algorithm is the simple epidemic routing.
+# [routing.sensor-mule-conf.routing]
+# algorithm = "epidemic"
+
       EOS
 
       etc.install "#{name}d.toml"
+
+      ohai "Created dtn7.toml config file at /usr/local/etc/#{name}d.toml"
     end
   end
 
@@ -88,6 +216,6 @@ class Dtn7 < Formula
     system "which", "#{name}d"
 
     # dtn7cat required a running dtn7d, thus use help as a simple test
-    system "#{name}cat", "help"
+    system "which", "#{name}-tool"
   end
 end
