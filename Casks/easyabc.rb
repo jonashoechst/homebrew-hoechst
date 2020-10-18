659cbadbb4f5384afdfeae3c519619c5b7ee38b0
Jonas Höchst <git@jonashoechst.de>
2020-10-18 11:12:33 +0200

    removed legacy software, updated easyabc

--- Casks/easyabc.rb
+++ Casks/easyabc.rb
@@ -1,8 +1,8 @@
 cask 'easyabc' do
-  version '1.3.7.7'
-  sha256 '9249693f3864872740b8666090a190bb6246fbe665195ce043ca4a7138d22b2a'
+  version '1.3.7.8beta64bit-1'
+  sha256 'c1e2b9b1ca7911e83d520d641272010592885d3f3072a2e94921e28d239f4ed2'
 
-  url 'https://iweb.dl.sourceforge.net/project/easyabc/EasyABC/1.3.7.7/EasyABC_1.3.7.7.dmg'
+  url 'https://brsn.org.uk/easyabc/EasyABC_1.3.7.8beta64bit-1.dmg'
   name 'EasyABC'
   homepage 'https://www.nilsliberg.se/ksp/easyabc/'
 
